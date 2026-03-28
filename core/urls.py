965c735ff370806a6391a8c6a8eab61c69475eff
Rohan Rahim <rohanrahim024@gmail.com>
2026-03-28 08:16:02 +0600

    Authentication added

---
+++ core/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    HomeView,
+    ProfileView,
+    LoginView,
+    LogoutView,
+    RegisterView,
+    PostsView
+)
+
+urlpatterns = [
+    path("", HomeView.as_view(), name='homepage'),
+    path("login/", LoginView.as_view(), name='login'),
+    path("register/", RegisterView.as_view(), name='register'),
+    path("logout/", LogoutView.as_view(), name='logout'),
+    path("profile/", ProfileView.as_view(), name='profile'),
+    path("posts/", PostsView.as_view(), name='posts'),
+]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,107 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
-class 
+class HomeView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'homepage.html')
+
+
+class ProfileView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'profile.html')
+    
+
+class PostsView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'posts.html')
+    
+
+class LoginView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect('homepage')
+        return render(request, 'login.html')
+    
+    def post(self, request: HttpRequest) -> HttpResponse:
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
+        else:
+            return render(
+                request,
+                'login.html',
+                context={'error': 'Invalid username or password!!'}
+            )
+    
+
+class RegisterView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect('homepage')
+        return render(request, 'register.html')
+    
+    def post(self, request: HttpRequest) -> HttpResponse:
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # adding validation
+        if len(password) < 4:
+            return render(
+                request,
+                'register.html',
+                context={'error': 'The password is too short!!'}
+            )
+
+        if password != confirm_password:
+            return render(
+                request,
+                'register.html',
+                context={'error': "The passwords don't match!!"}
+            )
+        
+        if User.objects.filter(username=username).exists():
+            return render(
+                request,
+                'register.html',
+                context={'error': "Username or Email already exists!!"}
+            )
+        
+        if User.objects.filter(email=email).exists():
+            return render(
+                request,
+                'register.html',
+                context={'error': "Username or Email already exists!!"}
+            )
+        
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        login(request, user)
+
+        return redirect('homepage')
+    
+
+class LogoutView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        logout(request)
+        return redirect('homepage')
+    
+
